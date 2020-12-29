292b6af1d0a652f118f327a72ba152676693fc50
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-12-29 09:59:01 +0530

    javascript

---
+++ JS/DataTypes/one.js
@@ -0,0 +1,19 @@
+var product_Name = "iphone 11";
+var price = 30000;
+var avilable = true;
+var store_Location;
+
+console.log(product_Name);
+console.log(price);
+console.log(avilable);
+console.log(store_Location);
+/*
+Required Software
+1.Node Js Software  https://nodejs.org/en/  download and install
+2.VS Code
+
+REPL 
+
+*/
+
+// Single Line comments
